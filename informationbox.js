f2eb77c6bc5ece06c43ea5f5fb447adda6403dd3
gloriakamidi <g.kamidi.312@westcliff.edu>
2022-09-27 18:37:08 -0700

    contact

--- informationbox.js
+++ informationbox.js
@@ -14,18 +14,20 @@ var textInfo = ["Friends for Life"];
 linkText = ["Click This To Close"];
 var txtLink = document.getElementById("textLink");
 txtLink.addEventListener("click", closeInfoBox);
+var infobox;
 
 
 //To Display Infobox
 //document.getElementsByClassName("description").addEventListener("click", displayInfoBox);
-document.querySelector("h2.description").addEventListener("click", displayInfoBox);
+document.querySelector(".description").addEventListener("click", displayInfoBox) = "gray";
 /*function infoBox {
     box = openTitle + title + closeTitle + openTextInfo + textInfo + closeTextInfo + 
 } */
+var infoBox = document.getElementById("infobox"); // get infobox
 function displayInfoBox() {
     document.getElementById("infobox").style.visibility = "visible";
     document.getElementById("title").innerHTML = captionText["pet1", "pet2", "pet3", "pet4", "pet5", "pet6", "pet7", "pet8", "pet9", "pet10"];
-    document.getElementsByClassName("textinfo").innerHTML = textInfo["Friends for Life"];
+    document.getElementsById("textinfo").innerHTML = textInfo["Friends for Life"];
     document.getElementById("textLink").innerHTML = linkText ["Click This To Close"];
 }
 
